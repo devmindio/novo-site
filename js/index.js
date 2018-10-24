b03d42d4a48ca9f3f43d1c5bd9b0567a2c73caac
Renan Mayrinck <renan.mtk@gmail.com>
2018-10-24 02:38:47 -0300

    .

--- js/index.js
+++ js/index.js
@@ -6,9 +6,13 @@ $(document).ready(function(){
 	$(document).scroll(function() {
 		var y = $(this).scrollTop();
 		if (y > 100) {
-			$('.goTop').fadeIn();
+			$(".goTop").fadeIn();
+            $(".first").css("padding-top", "79px");
+            $("header").addClass("detach");
 		} else {
-			$('.goTop').fadeOut();
+            $(".first").css("padding-top", "0px");
+            $("header").removeClass("detach");
+			$(".goTop").fadeOut();
 		}
 	}); 
 });
