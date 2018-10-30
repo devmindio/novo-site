e10ae08fc3ddc06794d348b8ee09122cea96c8a5
Renan Mayrinck <renan.mtk@gmail.com>
2018-10-30 19:06:32 -0300

    Icone do Gitlab Adicionado

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 		var y = $(this).scrollTop();
 		if (y > 100) {
 			$(".goTop").fadeIn();
-            $(".first").css("padding-top", "79px");
+            $(".first").css("padding-top", "100px");
             $("header").addClass("detach");
 		} else {
             $(".first").css("padding-top", "0px");
