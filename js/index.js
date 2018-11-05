82aae23e9fa5abe08130dddcf0a1bb34863086da
Renan Mayrinck <renan.mtk@gmail.com>
2018-11-05 04:57:10 -0200

    1234

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
-  $(".goTop").click(function(){
-		$("html, body").animate({ scrollTop: 0 }, 400);
-	});
-	
-	$(document).scroll(function() {
-		var y = $(this).scrollTop();
-		if (y > 100) {
-			$(".goTop").fadeIn();
-            $(".first").css("padding-top", "100px");
-            $("header").addClass("detach");
-		} else {
-            $(".first").css("padding-top", "0px");
-            $("header").removeClass("detach");
-			$(".goTop").fadeOut();
-		}
-	}); 
+    $(".goTop").click(function() {
+      $("html, body").animate({ scrollTop: 0 }, 400);
+    });
+
+    if ($(window).width() > 900) {
+        $(document).scroll(function() {
+            var y = $(this).scrollTop();
+            if (y > 100) {
+                $(".goTop").fadeIn();
+                //$(".first").css("padding-top", "100px");
+                $("header").addClass("detach");
+            } else {
+                //$(".first").css("padding-top", "0px");
+                $("header").removeClass("detach");
+                $(".goTop").fadeOut();
+            }
+        });
+    }
 });
