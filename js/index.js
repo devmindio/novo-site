c834dd214f0e0b0ecf7473ee7f98d080204592b6
Renan Mayrinck <renan.mtk@gmail.com>
2018-11-06 20:52:15 -0200

    começando os trabalhos na responsividade

--- js/index.js
+++ js/index.js
@@ -8,10 +8,8 @@ $(document).ready(function(){
             var y = $(this).scrollTop();
             if (y > 100) {
                 $(".goTop").fadeIn();
-                //$(".first").css("padding-top", "100px");
                 $("header").addClass("detach");
             } else {
-                //$(".first").css("padding-top", "0px");
                 $("header").removeClass("detach");
                 $(".goTop").fadeOut();
             }
